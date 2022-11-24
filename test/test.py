d05f7448e4f470d2045885b3c0b9f9bcccc18a57
davidkronish <david.kronish@mail.mcgill.ca>
2022-11-23 20:32:49 -0500

    added test files
    Change-Id: I49b6e816f6d0ef01fbbcc3aa3dcc7146af622c0e

---
+++ test/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestClassMethods(unittest.TestCase):
+    
+    def test_deck(self):
+        self.assertEqual(1,2)
+    
+
+if __name__ == "__main__":
+    unittest.main()
