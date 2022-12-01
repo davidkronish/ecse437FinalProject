024ff3528630a58f1efd5ccf329bb737cc43b9b7
davidkronish <david.kronish@mail.mcgill.ca>
2022-12-01 16:06:30 -0500

    Changed project structure to package sturcture, added setup for building it
    Change-Id: If8e0e75913304c86be11db583bcf49b8b34c4d51

---
+++ blackjack/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""
+here is a simple main() module -- to demonstrate setuptools entrypoints
+"""
+import sys, os
+from . import Deck, Hand
+
+help = """
+capitalize script
+
+capitalize file_to_process [output_file_name]
+
+capitalizes (title case) each line in a passed in file
+"""
+
+def main():
+    print("I'm done")
