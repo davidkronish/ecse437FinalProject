1355feaba9a837bdb68c83a98ad8e568e1efae7e
davidkronish <david.kronish@mail.mcgill.ca>
2022-12-01 17:16:14 -0500

    added card class
    Change-Id: I9b2dfa436013975809705a5cca6e6237300db597

---
+++ blackjack/card.py
@@ -0,0 +1,13 @@
+import sys
+class card:
+    suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+    ranks = ('Two', 'Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace')
+    
+    def __init__(self,suit,rank) -> None:
+        if suit not in self.suits or rank not in self.ranks:
+            raise Exception("The suit or rank is not valid entry.") 
+        self.suit = suit
+        self.rank = rank
+    
+    def __str__(self) -> str:
+        return self.rank+ " of "+self.suit
